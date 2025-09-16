57986c346625ce7de5852b6b09ac28e72a662aa2
Tammo-Tammisto <tammo.tammisto@tptlive.ee>
2025-09-17 01:26:26 +0300

    Throttle speed control

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,11 @@ int prevGearValue = -1;
 const int UP_THRESHOLD = 700;
 const int DOWN_THRESHOLD = 300;
 
+static bool stepState = false;
+
+const int MAX_DELAY = 1000;
+const int MIN_DELAY = 10;
+
 void setup()
 {
   Serial.begin(9600);
@@ -53,12 +58,74 @@ void loop()
   }
   prevGearValue = gearValue;
 
-  Serial.print("Gear Value: ");
-  Serial.print(gearValue);
-  Serial.print(" -> Current Gear: ");
-  Serial.println(currentGear);
-  Serial.print("Steering Value: ");
-  Serial.println(steeringValue);
-  Serial.print("Throttle Value: ");
-  Serial.println(throttleValue);
+  if (currentGear == DRIVE && throttleValue != 0)
+  {
+    digitalWrite(leftMotorDir, LOW);
+    digitalWrite(rightMotorDir, HIGH);
+
+    stepState = !stepState;
+    if (stepState)
+    {
+      digitalWrite(leftMotorStep, HIGH);
+      digitalWrite(rightMotorStep, LOW);
+    }
+    else
+    {
+      digitalWrite(leftMotorStep, LOW);
+      digitalWrite(rightMotorStep, HIGH);
+    }
+    delayMicroseconds(map(throttleValue, 0, 1023, MAX_DELAY, MIN_DELAY));
+  }
+  else if (currentGear == REVERSE && throttleValue != 0)
+  {
+    digitalWrite(leftMotorDir, HIGH);
+    digitalWrite(rightMotorDir, LOW);
+
+    stepState = !stepState;
+    if (stepState)
+    {
+      digitalWrite(leftMotorStep, HIGH);
+      digitalWrite(rightMotorStep, LOW);
+    }
+    else
+    {
+      digitalWrite(leftMotorStep, LOW);
+      digitalWrite(rightMotorStep, HIGH);
+    }
+    delayMicroseconds(map(throttleValue, 0, 1023, MAX_DELAY, MIN_DELAY));
+  }
+  else
+  {
+    digitalWrite(leftMotorStep, LOW);
+    digitalWrite(rightMotorStep, LOW);
+    delay(100);
+  }
+
+  // Serial.print("Gear Value: ");
+  // Serial.print(gearValue);
+  // Serial.print(" -> Current Gear: ");
+  // Serial.println(currentGear);
+  // Serial.print("Steering Value: ");
+  // Serial.println(steeringValue);
+  // Serial.print("Throttle Value: ");
+  // Serial.println(throttleValue);
 }
+
+// Sinu ülesandeks on kirjutada kood, mis vastab järgmistele nõuetele:
+//
+// ● Autol on kolm käiku, mille vahel saab vahetada käigukangile üles/alla vajutades:
+// ○ DRIVE – gaasi andes pöörlevad mootorid pärisuunas
+// ○ NEUTRAL – mootorid ei pöörle
+// ○ REVERSE – gaasi andes pöörlevad mootorid vastassuunas
+//
+// ● Gaasipedaali vajutades (potentsiomeetrit üles lohistades) saab kontrollida auto üldist kiirust
+//
+// ● Rooli keerates saab kontrollida auto kiiruse jaotust:
+// ○ Kui rool on otse, siis pöörlevad mootorid sama kiirusega
+// ○ Kui rooli keeratakse paremale, siis vasak mootor pöörleb kiiremini ja parem mootor aeglasemalt
+// ○ Kui rooli keeratakse vasakule, siis parem mootor pöörleb kiiremini ja vasak mootor aeglasemalt
+// ○ Mida rohkem rooli keeratakse, seda tugevam on mõju
+//
+// ● Mootorid pöörlevad sujuvalt ja nende töös ei toimu märgatavaid peatusi
+//
+// ● Ei tohi kasutada väliseid teeke
